49b56db1b3f24260ec8b572362039b58d9cbab19
Agatha <naufalmarjan2014@gmail.com>
2023-02-04 10:21:35 +0000

    spes: RiceDroid spesific flags

--- lineage_spes.mk
+++ lineage_spes.mk
@@ -11,24 +11,34 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from spes/spesn device
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
-# Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Additions
-$(call inherit-product-if-exists, vendor/gapps/common/common-vendor.mk)
-$(call inherit-product-if-exists, vendor/prebuilt-apps/config.mk)
-
-# Camera
-$(call inherit-product-if-exists, vendor/miuicamera/config.mk)
+# RiceDroid spesific flags
+RICE_MAINTAINER := Agatha
+RICE_CHIPSET := SM680
+SUSHI_BOOTANIMATION := 1080
+WITH_GMS := true
+TARGET_ENABLE_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_OPTOUT_GOOGLE_TELEPHONY := true
+TARGET_USE_PIXEL_FINGERPRINT := true
 
 # Product Specifics
-PRODUCT_NAME := arrow_spes
+PRODUCT_NAME := lineage_spes
 PRODUCT_DEVICE := spes
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 11
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="spes-user 11 RKQ1.211001.001 V13.0.9.0.RGCMIXM release-keys"
